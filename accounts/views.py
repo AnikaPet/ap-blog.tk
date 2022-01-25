24a077053965f782814f33e8930f49025d81e21e
AnikaPet <anika.petrovic1999@gmail.com>
2022-01-25 19:28:17 +0100

    Add form for adding new post.

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
-from cms import models
+from cms import models,forms
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
@@ -13,4 +13,9 @@ class SignUpView(generic.CreateView):
 def ProfileView(request):
     model = models.Post
     posts = model.objects.all()
-    return render(request,'userprofile.html',{'posts':posts})
+    form = forms.PostForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.author = request.user
+        obj.save()
+    return render(request,'userprofile.html',{'posts':posts,'form':form})
