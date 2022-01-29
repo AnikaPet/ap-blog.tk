f3a5edb10d90866de9e3cd9712a0db81671debd4
AnikaPet <anika.petrovic1999@gmail.com>
2022-01-29 18:14:09 +0100

    Update search view and add comments.

--- cms/views.py
+++ cms/views.py
@@ -3,36 +3,47 @@ from .models import Post
 from .forms import PostForm
 
 def about_us(request):
+    '''A view of about us page.'''
+
     return render(request,'about_us.html')
 
 def post_listing(request):
     '''A view of all blog posts.'''
+
     posts = Post.objects.all()  
     return render(request,'posts/post_listing.html',{'posts':posts})
 
-def post_details(request,post_id):  #dodajemo parametar koji je proslijedjen kroz url
+def post_details(request,post_id):
     '''A view of details of blog post.'''
+
     post = Post.objects.get(pk=post_id) 
     return render(request,'posts/post_detail.html',{'post':post})
 
 def post_search(request):
-    #imamo GET parametar ? ime par = vrijednost (razmaci su ili + ili %)
+    '''A view of posts searched by title.'''
+
     queried_post = request.GET['q']
-    post = None
-    try:
-        post = Post.objects.get(title = queried_post)
-        queried_post_exists = True
-    except:
+    posts = None
+
+    posts = Post.objects.filter(title__contains = queried_post)
+
+    if not posts:
         queried_post_exists = False
-    return render(request,'posts/post_searched.html',{'post':post,'results':queried_post_exists})
+    else:
+        queried_post_exists = True
+    
+    return render(request,'posts/post_searched.html',{'posts':posts,'results':queried_post_exists})
 
 def delete_post(request,post_id):
+    '''A view for deleting post.'''
+
     post = Post.objects.get(pk=post_id)
     post.delete()
     return render(request,'posts/delete_post.html',{'post':post})
 
 def add_post(request):
-    
+    '''A view for adding new post.'''
+
     posts = Post.objects.all()
     form = PostForm(request.POST or None)
     if form.is_valid():
@@ -44,7 +55,8 @@ def add_post(request):
     return render(request,'posts/add_post.html',{'posts':posts,'form':form})
 
 def edit_post(request,post_id):
-    
+    '''A view for editing post.'''
+
     posts = Post.objects.all()
     instance = Post.objects.get(pk=post_id)
     form = PostForm(request.POST or None,instance=instance)
